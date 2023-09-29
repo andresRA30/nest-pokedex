875eff5450fec622178bab88de31590bfc61e969
Andres <andres.robert@pfcti.com>
2023-09-29 09:28:23 -0600

    Fin seccion 7

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,13 @@
+import { IsInt, IsNumber, IsPositive, IsString, Min, MinLength, minLength } from "class-validator";
+
+export class CreatePokemonDto {
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+
+}
